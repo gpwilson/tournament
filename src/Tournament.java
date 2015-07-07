f13759c6e9e516a1ea3135d049ed29501577e080
gpwilson <gpwilson@centurylink.net>
2015-07-07 18:09:53 -0500

    7/7/2015 6:09 PM

---
+++ src/Tournament.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Glenn Wilson on 7/7/2015.
+ */
+public class Tournament {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the tournament program!");
+    }
+}
